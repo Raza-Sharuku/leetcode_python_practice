92e0712ddf9f1d6f37e7e662b7e7e375b8e74b05
Raza Shahrukh <126382610+Raza-Sharuku@users.noreply.github.com>
2024-08-24 13:45:47 +0200

    Time: 19 ms (74.33%), Space: 11.8 MB (32.85%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -1,12 +1,12 @@
 class Solution(object):#code
     def runningSum(self, nums):
         result = []
-        for i in range(0,len(nums)):
+        length = len(nums)
+        for i in range(0,length):
             if (i >= 1):
                 result.append(result[i - 1] + nums[i])
             else:
                 result.append(nums[i])
-        
         return result
 
 
