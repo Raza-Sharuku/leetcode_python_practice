334a3266d531ebeed20d1278e7433c95077b2043
Raza Shahrukh <126382610+Raza-Sharuku@users.noreply.github.com>
2024-08-26 12:42:16 +0200

    Time: 31 ms (52.09%), Space: 11.6 MB (82.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,13 +4,11 @@ class Solution(object):
         roman_dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         i = 0
         while i < len(s):
-            if i + 1 < len(s) and s[i] in roman_dict and roman_dict[s[i]] < roman_dict[s[i + 1]]:
+            if i + 1 < len(s) and roman_dict[s[i]] < roman_dict[s[i + 1]]:
                 result = result + (roman_dict[s[i + 1]] - roman_dict[s[i]])
                 i += 1
             else:
                 result += roman_dict[s[i]]
-            # print("result = " ,result)
-            # print(f"s[{i}] = {s[i]}")
             i += 1
             
         return result
